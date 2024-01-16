d40cfa5c7913589d411411aae6a417bd47a154c0
root <Part4@github.com>
2024-01-16 04:30:11 -0800

    changing the function insertion and add a swap function

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,28 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps two nodes in a doubly linked list.
+ * @list: A pointer to a pointer to the head of the doubly linked list.
+ * @node1: Pointer to the first node to swap.
+ * @node2: Pointer to the second node to swap.
+ */
+
+void swap(listint_t **list, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*list = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
+}
+
+/**
  * insertion_sort_list - Sorts a doubly linked list of integers using the
  *			insertion Sort algorithm.
  *
@@ -9,29 +31,17 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *temp;
+
 	if (list == NULL || *list == NULL)
 		return;
 
-	listint_t *current, *temp;
-
 	for (current = (*list)->next; current != NULL; current = current->next)
 	{
-		while (current->prev != NULL && current->n < current->prev->n)
+		temp = current->prev;
+		while (temp != NULL && temp->n > current->n)
 		{
-			temp = current->prev;
-			current->prev = temp->prev;
-			temp->next = current->next;
-			temp->prev = current;
-			current->next = temp;
-
-			if (temp->next != NULL)
-				temp->next->prev = temp;
-
-			if (current->prev != NULL)
-				current->prev->next = current;
-			else
-				*list = current;
-
+			swap(list, &temp, current);
 			print_list(*list);
 		}
 	}
